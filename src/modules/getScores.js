f29b33b459ccd1ea60f4aa99cbccbf9754879c8b
Jake Felice <jasonfelice0@gmail.com>
2022-05-11 06:58:47 -0700

    Shorten getScores code to 2 lines

--- src/modules/getScores.js
+++ src/modules/getScores.js
@@ -1,6 +1,4 @@
 export default async () => {
   const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/2E64OsG4g1JjcstMkAMd/scores';
-  const response = await fetch(url);
-  const users = await response.json();
-  return users;
+  return (await fetch(url)).json();
 };
