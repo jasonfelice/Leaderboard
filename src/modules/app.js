e6113f86467b3a5e8175805263a64eafd58944f8
Jake Felice <jasonfelice0@gmail.com>
2022-05-11 06:48:18 -0700

    Add function that fetches scores from an api

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,8 +1,28 @@
 import addScore from './addScore.js';
+import getScores from './getScores.js';
 
 const nameField = document.getElementById('userName');
 const scoreField = document.getElementById('userScore');
 const submitScore = document.getElementById('submitScore');
+const refreshButton = document.getElementById('refresh');
+
+refreshButton.addEventListener('click', async () => {
+  let userScores = [];
+  try {
+    userScores = await getScores();
+  } catch (e) {
+    return `Error! ${e}`;
+  }
+  if (userScores.result.length > 0) {
+    userScores.result.forEach((arr) => {
+      addScore({
+        name: arr.user,
+        score: arr.score,
+      });
+    });
+  }
+  return 'Added scores';
+});
 
 submitScore.addEventListener('click', (e) => {
   e.preventDefault();
